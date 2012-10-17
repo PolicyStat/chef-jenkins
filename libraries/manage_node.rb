5e1cdf168528a3f5f59ac282a4c6eb40fc841248
winhamwr <winhamwr@gmail.com>
2012-10-17 18:38:32 -0400

    Added work-around for CHEF-3218 auto-gzip bug.

--- libraries/manage_node.rb
+++ libraries/manage_node.rb
@@ -11,9 +11,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -155,4 +155,16 @@ if File.basename($0) == File.basename(__FILE__)
     args[k.to_sym] = v
   end
   puts jenkins_node_manage(args)
+end
+
+# Work-around for the auto-gzip problem described here:
+# http://tickets.opscode.com/browse/CHEF-3218
+class Chef
+  class Provider
+    class HttpRequest < Chef::Provider
+      def load_current_resource
+        @rest = Chef::REST.new(@new_resource.url, nil, nil, :disable_gzip => true)
+      end
+    end
+  end
 end
